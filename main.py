91ec5ea305289e8bb3223b3c2b6297e6a10ae832
ogiogidayo <eiji.ogi0326@icloud.com>
2023-09-29 23:34:25 +0900

    view修正

--- main.py
+++ main.py
@@ -15,11 +15,7 @@ def get_db():
         yield db
     finally:
         db.close()
-# @app.get("/")
-# async def index():
-#     return {"message": "Success"}
 
-# Read
 @app.get("/users", response_model=List[schemas.User])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
